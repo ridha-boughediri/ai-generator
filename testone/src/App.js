139156868cdb4ed3b2f041603ac04732fc5022d9
ridha-boughediri <ridha.boughediri@laplateforme.io>
2023-06-11 14:08:04 +0200

    mise en page des inputs

--- testone/src/App.js
+++ testone/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import ImageGene from "./components/Imagegerate/ImageGene";
+import TextGene from "./components/Textgerate/TextGene";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className="w-2/12">test1</div>
+      <div className="w-8/12 justify-center items-center ">
+        <div className="flex justify-center items-center">
+          <h1 className="text-4xl font-bold text-center">Generator</h1>
+        </div>
+        <TextGene />
+
+        <ImageGene />
+      </div>
     </div>
   );
 }
--- testone/src/components/Imagegerate/ImageGene.js
+++ testone/src/components/Imagegerate/ImageGene.js
@@ -1,7 +1,56 @@
-import React from "react";
+import { useState } from "react";
 
-function ImageGene() {
-  return <div>hello world image</div>;
-}
+const ImageGen = () => {
+  const [generatedImage, setGeneratedImage] = useState(null);
+  const [inputValue, setInputValue] = useState("");
 
-export default ImageGene;
+  const handleGenerateImage = async () => {
+    // Call the OpenAI API to generate the image based on the inputValue
+    // Replace `yourOpenAIApiCall` with the actual code to generate the image using OpenAI
+    // const generatedImage = await yourOpenAIApiCall(inputValue);
+
+    // Update the state with the generated image
+    setGeneratedImage(generatedImage);
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <h2 className="text-4xl font-extrabold dark:text-white">
+        Generer une image
+      </h2>
+
+      <form>
+        {/* Your form inputs here */}
+        <div class="relative mb-3" data-te-input-wrapper-init>
+          <textarea
+            id="comment"
+            rows="4"
+            class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+            placeholder="Write a comment..."
+            required
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <button
+          onClick={handleGenerateImage}
+          type="submit"
+          class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+        >
+          Generate Image
+        </button>
+
+        {/* Display the generated image */}
+        {generatedImage && <img src={generatedImage} alt="Generated" />}
+      </form>
+    </>
+  );
+};
+
+export default ImageGen;
