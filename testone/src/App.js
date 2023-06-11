1bf65a3d203f0dc56bbe2381386042820170d94f
ridha-boughediri <ridha.boughediri@laplateforme.io>
2023-06-11 15:29:09 +0200

    prepa imag

--- testone/src/App.js
+++ testone/src/App.js
@@ -4,13 +4,11 @@ import TextGene from "./components/Textgerate/TextGene";
 function App() {
   return (
     <div className="">
-      <div className="w-2/12">test1</div>
       <div className="w-8/12 justify-center items-center ">
         <div className="flex justify-center items-center">
           <h1 className="text-4xl font-bold text-center">Generator</h1>
         </div>
         <TextGene />
-
         <ImageGene />
       </div>
     </div>
--- testone/src/components/Imagegerate/ImageGene.js
+++ testone/src/components/Imagegerate/ImageGene.js
@@ -1,16 +1,35 @@
 import { useState } from "react";
 
 const ImageGen = () => {
-  const [generatedImage, setGeneratedImage] = useState(null);
+  const [generatedImage, setGeneratedImage] = useState(
+    "https://placehold.co/600x400"
+  );
   const [inputValue, setInputValue] = useState("");
 
   const handleGenerateImage = async () => {
-    // Call the OpenAI API to generate the image based on the inputValue
-    // Replace `yourOpenAIApiCall` with the actual code to generate the image using OpenAI
-    // const generatedImage = await yourOpenAIApiCall(inputValue);
+    try {
+      // Call the OpenAI API to generate the image based on the inputValue
+      const response = await fetch("yourOpenAIApiEndpoint", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Add any required headers for your API
+        },
+        body: JSON.stringify({ inputValue }),
+      });
 
-    // Update the state with the generated image
-    setGeneratedImage(generatedImage);
+      if (!response.ok) {
+        throw new Error("Failed to generate image");
+      }
+
+      const generatedImage = await response.json();
+
+      // Update the state with the generated image
+      setGeneratedImage(generatedImage);
+    } catch (error) {
+      console.error(error);
+      // Handle the error appropriately (e.g., show an error message)
+    }
   };
 
   const handleInputChange = (e) => {
@@ -20,16 +39,16 @@ const ImageGen = () => {
   return (
     <>
       <h2 className="text-4xl font-extrabold dark:text-white">
-        Generer une image
+        Generate an Image
       </h2>
 
       <form>
         {/* Your form inputs here */}
-        <div class="relative mb-3" data-te-input-wrapper-init>
+        <div className="relative mb-3" data-te-input-wrapper-init>
           <textarea
             id="comment"
             rows="4"
-            class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+            className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
             placeholder="Write a comment..."
             required
             type="text"
@@ -40,8 +59,8 @@ const ImageGen = () => {
 
         <button
           onClick={handleGenerateImage}
-          type="submit"
-          class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+          type="button"
+          className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
         >
           Generate Image
         </button>
